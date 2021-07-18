c3a2acd3acb42a3d01dfec441256208d55c1730d
kennetha123 <jodysag5@gmail.com>
2021-07-19 06:27:03 +0700

    Fix camera restarting position to center when right click mouse again

--- Engine/Source/OpenGL/OpenGLInput.cpp
+++ Engine/Source/OpenGL/OpenGLInput.cpp
@@ -1,6 +1,15 @@
 #include "actapch.h"
 #include "OpenGLInput.h"
 
+extern unsigned int windowWidth;
+extern unsigned int windowHeight;
+extern float yaw;
+extern float pitch;
+
+static bool firstMouse = true;
+static float lastMousePosX = windowWidth / 2;
+static float lastMousePosY = windowHeight / 2;
+
 void OpenGLInput::ProcessInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -16,8 +25,10 @@ void OpenGLInput::ProcessInput(GLFWwindow* window)
 	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) != GLFW_PRESS)
 	{
 		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-		glfwSetCursorPosCallback(window, [](GLFWwindow* window, double xPos, double yPos) {});
+		glfwSetCursorPosCallback(window, [](GLFWwindow* window, double xPos, double yPos) { firstMouse = true; });
 	}
+
+	spdlog::info("Last Pos X : {0}, Last Pos Y : {1}", lastMousePosX, lastMousePosY);
 }
 
 void OpenGLInput::ProcessInput(GLFWwindow* window, GLenum glKey, GLenum glPress, std::function<void()> func)
@@ -28,15 +39,6 @@ void OpenGLInput::ProcessInput(GLFWwindow* window, GLenum glKey, GLenum glPress,
 	}
 }
 
-extern unsigned int windowWidth;
-extern unsigned int windowHeight;
-extern float yaw;
-extern float pitch;
-
-static bool firstMouse = true;
-static float lastMousePosX = windowWidth / 2;
-static float lastMousePosY = windowHeight / 2;
-
 void mouse_callback(GLFWwindow* window, double xPos, double yPos)
 {
 	if (firstMouse) // initially set to true
@@ -45,7 +47,6 @@ void mouse_callback(GLFWwindow* window, double xPos, double yPos)
 		lastMousePosY = yPos;
 		firstMouse = false;
 	}
-
 
 	float xOffset = xPos - lastMousePosX;
 	float yOffset = lastMousePosY - yPos;
