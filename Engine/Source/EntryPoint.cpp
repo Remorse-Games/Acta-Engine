b955b9b764027a79e84d60c0ee5fb2460f997743
ZivFerdinand <zivenf@gmail.com>
2021-07-18 21:03:11 +0700

    UnitTest Change (Gtest > Catch)
    https://remorse-studio.atlassian.net/browse/AE-13

--- Engine/Source/EntryPoint.cpp
+++ Engine/Source/EntryPoint.cpp
@@ -1,9 +1,23 @@
 #include "actapch.h"
 #include "OpenGL/OpenGLWindow.h"
+//TEST(InitialTest, JustAnExample)
+//{
+//	EXPECT_EQ(0, 0);
+//}
+#define CATCH_CONFIG_MAIN
+#include "../../Externals/catch.hpp"
 
-int main()
+#define CATCH_CONFIG_MAIN
+#define UNIT_TESTING 1
+
+#if !UNIT_TESTING
+int main(int argc, char** argv)
 {
 	OpenGLWindow* window = new OpenGLWindow(1280, 720, "Remorse Render Engine");
 	window->UpdateWindow();
 	delete window;
 }
+#endif
+
+
+
--- Engine/Source/actapch.cpp
+++ Engine/Source/actapch.cpp
@@ -1,1 +1,10 @@
 #include "actapch.h"
+#include "../catch.hpp"
+
+TEST_CASE("FileManager", "FileManagerTesting")
+{
+	SECTION("FileCountTest")
+	{
+		REQUIRE(0 == 0);
+	}
+}
