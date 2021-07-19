4d9645df6d5d0eec491fc3655caa3282c4bb20a1
ZivFerdinand <zivenf@gmail.com>
2021-07-19 15:27:10 +0700

    Deleted Comments And Unrelevant Things
    https://remorse-studio.atlassian.net/secure/RapidBoard.jspa?rapidView=1&projectKey=AE&modal=detail&selectedIssue=AE-13

--- Engine/Source/EntryPoint.cpp
+++ Engine/Source/EntryPoint.cpp
@@ -1,13 +1,9 @@
 #include "actapch.h"
 #include "OpenGL/OpenGLWindow.h"
-//TEST(InitialTest, JustAnExample)
-//{
-//	EXPECT_EQ(0, 0);
-//}
+
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-#define CATCH_CONFIG_MAIN
 #define UNIT_TESTING 1
 
 #if !UNIT_TESTING
