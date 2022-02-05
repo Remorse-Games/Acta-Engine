b592aa20c684019ab5a0d370e029efd83d30dce8
Jody Ruben <jody@accelbyte.net>
2022-02-05 16:37:29 +0700

    add one more line in remorse engine.bat

--- Generate_Projects.bat
+++ Generate_Projects.bat
@@ -1,8 +1,8 @@
 @echo off
 
-
 echo ===============================================
-echo ========REMORSE ENGINE SETUP  ================
+echo ===============================================
+echo ========REMORSE ENGINE SETUP  =================
 echo ===============================================
 
 call Tools\premake\premake5.exe --file=Make/premake5.lua vs2019
